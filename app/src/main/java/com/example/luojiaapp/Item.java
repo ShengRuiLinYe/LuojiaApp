4f7634efeb99a9cab76a3a9fb5ce51f508581fe3
MMMMMMoSky <q94354906@gmail.com>
2019-12-20 19:30:18 +0800

    修改了变量名: sales -> amount

--- app/src/main/java/com/example/luojiaapp/Item.java
+++ app/src/main/java/com/example/luojiaapp/Item.java
@@ -8,7 +8,7 @@ public class Item {
     private int imageId2;
     private Bitmap bitmap;
     private int price;
-    private int sales;
+    private int amount;
     private int category;
 
     final static int book = 1;
@@ -21,16 +21,16 @@ public class Item {
         this.imageId = imageId;
         imageId2 = R.drawable.saber;
         this.price = -1;
-        this.sales = -1;
+        this.amount = -1;
         this.category = other;
     }
 
-    public Item(String name, int imageId, int imageId2, int price, int sales, int category) {
+    public Item(String name, int imageId, int imageId2, int price, int amount, int category) {
         this.name = name;
         this.imageId = imageId;
         this.imageId2 = imageId2;
         this.price = price;
-        this.sales = sales;
+        this.amount = amount;
         this.category = category;
     }
 
@@ -42,13 +42,13 @@ public class Item {
     }
     public int getImageId2() {return imageId2;}
     public int getPrice(){return price;}
-    public int getSales(){return sales;}
+    public int getAmount(){return amount;}
     public int getCategory() {return category;}
     public Bitmap getBitmap() {return bitmap;}
 
     public void setName(String name){this.name = name;}
     public void setPrice(int price) {this.price = price;}
-    public void setSales(int sales) {this.sales = sales;}
+    public void setAmount(int amount) {this.amount = amount;}
     public void setCategory(int category) {this.category = category;}
     public void setImageId(int imageId) {this.imageId = imageId;}
     public void setImageId2(int imageId2) {this.imageId2 = imageId2;}
