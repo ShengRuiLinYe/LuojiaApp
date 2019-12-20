d906ba03fe742e31c183e4c88bbf6bf06a29742b
MMMMMMoSky <q94354906@gmail.com>
2019-12-20 19:31:51 +0800

    添加商品, 向服务器传输数据

--- app/src/main/java/com/example/luojiaapp/ServerConnection.java
+++ app/src/main/java/com/example/luojiaapp/ServerConnection.java
@@ -4,18 +4,18 @@
  */
 package com.example.luojiaapp;
 
-import android.provider.ContactsContract;
-import android.util.Log;
-import android.widget.Toast;
+import android.graphics.Bitmap;
 
-import java.net.*;
-import java.io.*;
+import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.Socket;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class ServerConnection {
-    private static String serverIP = "10.131.168.166";
+    private static String serverIP = "10.131.138.53";
     private static int serverPort = 12345;
 
     /**
@@ -37,13 +37,13 @@ public class ServerConnection {
                     byte[] buf = new byte[1024];
 
                     out.write("verify password".getBytes("utf-8"));
-                    Arrays.fill(buf, (byte)0);
+                    Arrays.fill(buf, (byte) 0);
                     in.read(buf);
                     out.write(userid.getBytes("utf-8"));
-                    Arrays.fill(buf, (byte)0);
+                    Arrays.fill(buf, (byte) 0);
                     in.read(buf);
                     out.write(password.getBytes("utf-8"));
-                    Arrays.fill(buf, (byte)0);
+                    Arrays.fill(buf, (byte) 0);
                     in.read(buf);
 
                     String username = (new String(buf, "utf-8")).trim();
@@ -74,6 +74,7 @@ public class ServerConnection {
 
     /**
      * 注册用户 提供用户昵称和密码, 返回用户id, 注册失败则返回 null
+     *
      * @param username
      * @param password
      * @return 用户id
@@ -90,13 +91,13 @@ public class ServerConnection {
                     byte[] buf = new byte[1024];
 
                     out.write("register common user".getBytes("utf-8"));
-                    Arrays.fill(buf, (byte)0);
+                    Arrays.fill(buf, (byte) 0);
                     in.read(buf);
                     out.write(username.getBytes("utf-8"));
-                    Arrays.fill(buf, (byte)0);
+                    Arrays.fill(buf, (byte) 0);
                     in.read(buf);
                     out.write(password.getBytes("utf-8"));
-                    Arrays.fill(buf, (byte)0);
+                    Arrays.fill(buf, (byte) 0);
                     in.read(buf);
 
                     String userid = (new String(buf, "utf-8")).trim();
@@ -129,6 +130,7 @@ public class ServerConnection {
     /**
      * 获取当前用户的商人信息, 若该用户还未注册成为商人, 返回 null
      * 发送的是已经登陆的用户id, 这个方法不应该在未登陆的时候调用
+     *
      * @return 商人信息 "%s;%s;%s;%s;%s" % (姓名, 联系方式, 学部, 院系, 年级)
      */
     public static String getSellerInfo() {
@@ -147,10 +149,10 @@ public class ServerConnection {
                     byte[] buf = new byte[1024];
 
                     out.write("get seller info".getBytes("utf-8"));
-                    Arrays.fill(buf, (byte)0);
+                    Arrays.fill(buf, (byte) 0);
                     in.read(buf);
                     out.write(LoginStatus.userid.getBytes("utf-8"));
-                    Arrays.fill(buf, (byte)0);
+                    Arrays.fill(buf, (byte) 0);
                     in.read(buf);
 
                     String info = (new String(buf, "utf-8")).trim();
@@ -182,6 +184,7 @@ public class ServerConnection {
     /**
      * 注册成为商人
      * 发送的是已经登陆的用户id, 这个方法不应该在未登陆的时候调用
+     *
      * @param sellerinfo 注册信息, 格式: "%s;%s;%s;%s;%s" % (姓名, 联系方式, 学部, 院系, 年级)
      * @return 注册是否成功
      */
@@ -197,13 +200,13 @@ public class ServerConnection {
                     byte[] buf = new byte[1024];
 
                     out.write("register seller".getBytes("utf-8"));
-                    Arrays.fill(buf, (byte)0);
+                    Arrays.fill(buf, (byte) 0);
                     in.read(buf);
                     out.write(LoginStatus.userid.getBytes("utf-8"));
-                    Arrays.fill(buf, (byte)0);
+                    Arrays.fill(buf, (byte) 0);
                     in.read(buf);
                     out.write(sellerinfo.getBytes("utf-8"));
-                    Arrays.fill(buf, (byte)0);
+                    Arrays.fill(buf, (byte) 0);
                     in.read(buf);
 
                     String res = (new String(buf, "utf-8")).trim();
@@ -228,5 +231,79 @@ public class ServerConnection {
         }
 
         return ret.get(0);
+    }
+
+    /**
+     * 卖家上传一个新的商品
+     *
+     * @param item 商品, 具体内容见 Item.java, 需要将其所有的信息上传至服务器
+     */
+    public static boolean sellerAddItem(final Item item) {
+        final List<Boolean> ret = new ArrayList<>();
+        Runnable sai = new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Socket socket = new Socket(serverIP, serverPort);
+                    DataInputStream in = new DataInputStream(socket.getInputStream());
+                    DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+                    byte[] buf = new byte[1024];
+
+                    // 0. 发送控制信息: seller add item
+                    out.write("seller add item".getBytes("utf-8"));
+                    Arrays.fill(buf, (byte) 0);
+                    in.read(buf);
+
+                    // 1. 发送 userid, 表示商品属主
+                    out.write(LoginStatus.userid.getBytes("utf-8"));
+                    Arrays.fill(buf, (byte) 0);
+                    in.read(buf);
+
+                    // 2. 发送商品基本信息, 字符串格式为 "%s;%s;%s;%s"
+                    //  表示 商品名称;商品价格;商品数量;商品类型编号
+                    String info = item.getName() + ";" + item.getPrice() + ";" +
+                            item.getAmount() + ";" + item.getCategory();
+                    out.write(info.getBytes("utf-8"));
+                    Arrays.fill(buf, (byte) 0);
+                    in.read(buf);
+
+                    // 将商品图片 Bitmap 转换成 byte[] 数组
+                    byte[] pic = PicProcess.bitmap2bytes(item.getBitmap());
+                    // 3. 发送商品图片的字节长度
+                    out.write(String.valueOf(pic.length).getBytes("utf-8"));
+                    Arrays.fill(buf, (byte) 0);
+                    in.read(buf);
+
+                    // 4. 分批发送商品图片, 每次发送 1024 bytes, 最后一次可能不足 1024 bytes, 可以根据上一次发送的数字计算
+                    for (int i = 0; i < pic.length; i += 1024) {
+                        out.write(pic, i, Math.min(1024, pic.length - i));
+                        Arrays.fill(buf, (byte) 0);
+                        in.read(buf);
+                    }
+
+                    // 5. 最后一次读取到的内容应该为 true, 表示该商品已经添加成功
+                    String res = (new String(buf, "utf-8")).trim();
+                    ret.add("true".equals(res));
+                } catch (Exception e) {
+                }
+            }
+        };
+
+        Thread thread = new Thread(sai);
+        thread.start();
+        try {
+            thread.join();
+        }
+        catch (Exception e) {}
+
+        return ret.size() == 1 && ret.get(0);
+    }
+}
+
+class PicProcess {
+    public static byte[] bitmap2bytes(Bitmap bm) {
+        ByteArrayOutputStream s = new ByteArrayOutputStream();
+        bm.compress(Bitmap.CompressFormat.PNG, 100, s);
+        return s.toByteArray();
     }
 }
